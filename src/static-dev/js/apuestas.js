f1c3dc14a8269a174cc27c547b011287eadde3b0
Pablo N Dalmasso <pablodalmasso@gmail.com>
2017-09-01 11:25:07 -0300

    Se agrega ajax, en una forma muy sencilla

--- src/static-dev/js/apuestas.js
+++ src/static-dev/js/apuestas.js
@@ -1,4 +1,23 @@
 
-function send_apuesta(respuesta_valida_id){
-    alert(respuesta_valida_id)
+function open_modal_register(){
+    $('#modal_register').modal('show');
+}
+
+function cancel_modal_register(){
+    $('#modal_register').find('#id_username').val('');
+    $('#modal_register').find('#id_password1').val('');
+    $('#modal_register').find('#id_password2').val('');
+    $('#modal_register').modal('hide');
+}
+
+function send_register_form(){
+
+    $.ajax({
+        type: "POST",
+        url: $('#id_form_register').attr('action'),
+        data: $('#id_form_register').serialize(),
+        success: function(response){
+            alert(response)
+        }
+    })
 }
